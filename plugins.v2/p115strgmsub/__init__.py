8859f9dcc180037bd11228401a96fa515d1df9da
zza774886285 <zza774886285@users.noreply.github.com>
2026-08-02 09:20:32 +0800

    bump version to 1.6.0

--- plugins.v2/p115strgmsub/__init__.py
+++ plugins.v2/p115strgmsub/__init__.py
@@ -38,7 +38,7 @@ class P115StrgmSub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.5.3"
+    plugin_version = "1.6.0"
     # 插件作者
     plugin_author = "mrtian2016"
     # 作者主页
